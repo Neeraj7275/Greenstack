aca0260cad971e940568f04513d0f9e2f3e7b1bb
Neeraj7275 <neeraj17022004@gmail.com>
2025-06-20 20:34:09 +0530

    new one for test

--- server/config/db.js
+++ server/config/db.js
@@ -2,12 +2,8 @@ import mongoose from "mongoose";
 
 const dbconnection = async ()=>{
     try {
-        mongoose.connection.on('connected', ()=>console.log("data base connected"))
-        await mongoose.connect(`${process.env.MONGODB_URI}/greenstack`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  bufferTimeoutMS: 30000, // Increase to 30 seconds (30000ms)
-});
+        mongoose.connection.on('connected', ()=>console.log("data base connected"));
+        await mongoose.connect(`${process.env.MONGODB_URI}/greenstack`);
         
     } catch (error) {
         console.log("db connection failed",error); 
