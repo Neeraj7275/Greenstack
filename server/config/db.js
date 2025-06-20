1bd23f5cdb4d063497594223d5d412411c084c0a
Neeraj7275 <neeraj17022004@gmail.com>
2025-06-20 20:15:31 +0530

    try to solve error

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,11 @@ import mongoose from "mongoose";
 const dbconnection = async ()=>{
     try {
         mongoose.connection.on('connected', ()=>console.log("data base connected"))
-        await mongoose.connect(`${process.env.MONGODBURI}/greencart`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/greenstack`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  bufferTimeoutMS: 30000, // Increase to 30 seconds (30000ms)
+});
         
     } catch (error) {
         console.log("db connection failed",error); 
