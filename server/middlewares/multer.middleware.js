6c625ca8904ca69f08c4234481e06169069a50d1
Neeraj7275 <neeraj17022004@gmail.com>
2025-06-22 17:53:00 +0530

    updated multer file

--- server/middlewares/multer.middleware.js
+++ server/middlewares/multer.middleware.js
@@ -1,15 +1,22 @@
+// import multer from "multer";
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, "./public/temp")
+//     },
+//     filename: function (req, file, cb) {
+      
+//       cb(null, file.originalname)
+//     }
+//   })
+
 import multer from "multer";
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "./public/temp")
-    },
-    filename: function (req, file, cb) {
-      
-      cb(null, file.originalname)
-    }
-  })
+const storage = multer.memoryStorage(); // store file in memory
+
+export const upload = multer({ storage });
+
   
-export const upload = multer({ 
-    storage, 
-})
+// export const upload = multer({ 
+//     storage, 
+// })
